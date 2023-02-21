7a9242dd09413b99780145d4534d121957247fb7
KrupaShivanna <115063947+KrupaShivanna@users.noreply.github.com>
2023-02-21 17:12:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import DashBoard from "./components/dashboard";
+import './style/db.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DashBoard />
+    </div>
+  );
+}
+
+export default App;
